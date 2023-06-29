79d6f03e309e7970ddb1bc78ec0eb1f368f3cc0c
danilopgon <113547781+danilopgon@users.noreply.github.com>
2023-06-29 10:51:21 +0000

    Modify character properties

--- src/models.py
+++ src/models.py
@@ -14,17 +14,19 @@ class User(Base):
     nickname = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
 
+
 class Planet(Base):
     __tablename__ = "planet"
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
     url = Column(String, nullable=False, unique=True)
 
+
 class Planet_Properties(Base):
     __tablename__ = "planet_properties"
     id = Column(Integer, primary_key=True)
-    planet_id = Column(ForeignKey('planet.id'))
-    name = Column(ForeignKey('planet.name'))
+    planet_id = Column(ForeignKey("planet.id"))
+    name = Column(ForeignKey("planet.name"))
     climate = Column(String, nullable=False)
     created = Column(String, nullable=False)
     diameter = Column(String, nullable=False)
@@ -36,8 +38,8 @@ class Planet_Properties(Base):
     rotation_period = Column(Integer, nullable=False)
     surface_water = Column(Integer, nullable=False)
     terrain = Column(String, nullable=False)
-    created=Column(DateTime, nullable=False)
-    edited= Column(DateTime, nullable=False)
+    created = Column(DateTime, nullable=False)
+    edited = Column(DateTime, nullable=False)
     url = Column(String, nullable=False, unique=True)
 
 
@@ -45,13 +47,14 @@ class Vehicle(Base):
     __tablename__ = "vehicle"
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
-    url = Column(String, nullable=False, unique=True)  
+    url = Column(String, nullable=False, unique=True)
+
 
 class Vehicle_Properties(Base):
     __tablename__ = "vehicle_properties"
     id = Column(Integer, primary_key=True)
-    vehicle_id = Column(ForeignKey('vehicle.id'))
-    name = Column(ForeignKey('vehicle.name'))
+    vehicle_id = Column(ForeignKey("vehicle.id"))
+    name = Column(ForeignKey("vehicle.name"))
     cargo_capacity = Column(Integer, nullable=False)
     created = Column(String, nullable=False)
     crew = Column(Integer, nullable=False)
@@ -64,11 +67,12 @@ class Vehicle_Properties(Base):
     pilots = Column(String, nullable=False)
     films = Column(String, nullable=False)
     url = Column(String, nullable=False)
-    created=Column(DateTime, nullable=False)
-    edited= Column(DateTime, nullable=False) 
+    created = Column(DateTime, nullable=False)
+    edited = Column(DateTime, nullable=False)
+
 
 class Character(Base):
-    __table_name__ = 'Character'
+    __table_name__ = "Character"
     id = Column(Integer, primary_key=True)
     name = Column(String)
     url = Column(String)
@@ -76,19 +80,22 @@ class Character(Base):
 
 class Character_Properties(Base):
     id = Column(Integer, primary_key=True)
-    character_id = Column(ForeignKey('Character.id'))
-    cargo_capacity = Column(String, nullable=False)
-    consumables = Column(String, nullable=False)
-    crew = Column(String, nullable=False)
-    cargo_manufacturercapacity = Column(String, nullable=False)
-    max_atmosphering_speed = Column(String, nullable=False)
-    pilots = Column(String, nullable=False)
-    passengers = Column(String, nullable=False)
+    character_id = Column(ForeignKey("Character.id"))
+    height = Column(Integer, nullable=False)
+    mass = Column(Integer, nullable=False)
+    hair_color = Column(String, nullable=False)
+    skin_color = Column(String, nullable=False)
+    eye_color = Column(String, nullable=False)
+    birth_year = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+    homeworld = Column(String, nullable=False)
     films = Column(String, nullable=False)
-    vehicle_class = Column(String, nullable=False)
-    url = Column(String, nullable=False)
+    species = Column(String, nullable=False)
+    vehicles = Column(String, nullable=False)
+    starships = Column(String, nullable=False)
     created = Column(DateTime)
     edited = Column(DateTime)
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, "diagram.png")
